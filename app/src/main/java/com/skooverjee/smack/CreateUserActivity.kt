2ab3f87ff7d3cec1971edfb5818a639ba499b07e
Shaheel Kooverjee <skooverjee@gmail.com>
2020-01-03 10:22:54 +0200

    Add Create User activity and screen layout

---
+++ app/src/main/java/com/skooverjee/smack/CreateUserActivity.kt
@@ -0,0 +1,26 @@
+package com.skooverjee.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateBackgroundColourBtnClicked(view: View) {
+
+    }
+
+    fun createUserBtnClicked(view: View) {
+
+    }
+
+}
